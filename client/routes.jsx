50daa1ed36bdf22b34ed2fabf323b7be309bef90
Jimmie Butler <jimmiebtlr@gmail.com>
2015-10-10 16:04:43 -0500

    Example of styling.

--- client/routes.jsx
+++ client/routes.jsx
@@ -10,7 +10,37 @@ App = React.createClass({
   }
 });
 
-HelloWorld = React.createClass({
+Styles = {
+  helloworld: { 
+    'fontSize': "50px",
+    'color': 'blue'
+  }
+};
+
+Styleable = function( component ){
+  component.prototype.styles = function( name, defaults ){
+    var styles = [];
+
+    if( defaults ){
+      styles.push( defaults );
+    }else if( this.styles ){
+      styles.push( this.defaultStyles ); 
+    }
+
+    if( name ){
+      styles.push( Styles[ name ] );
+    }if( this.name ){
+      styles.push( Styles[ this.name ] );
+    }
+
+    return  styles;
+  }
+
+  return Radium( component );
+}
+
+HelloWorld = Styleable(React.createClass({
+  name: "helloworld",
   mixin: [ReactMeteorData],
   
   getMeteorData: function(){
@@ -19,16 +49,15 @@ HelloWorld = React.createClass({
   },
 
   render(){
-    return (<h2>Hello world</h2>);
+    console.log( this );
+    return (<h2 style={this.styles()}>Hello world</h2>);
   }
-});
+}));
 
 Meteor.startup(function() {
   React.render((
-      <Router history={browserHistory}>
-        <Route path="/" component={App}>
-          <Route path="/helloWorld" component={HelloWorld}/>
-        </Route>
-      </Router>
+    <Router history={browserHistory}>
+      <Route path="/helloWorld" component={HelloWorld}/>
+    </Router>
   ),document.getElementsByTagName('body')[0]);
 });
