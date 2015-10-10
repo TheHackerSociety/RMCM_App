2aec10e17ef8a564526670de0ced0a0c3b2ff5dd
ninja <ksepehri@users.noreply.github.com>
2015-10-10 17:55:08 -0500

    added publications and data fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -6,7 +6,143 @@ if (Events.find().count() === 0) {
         name: 'RP Harris Elementary',
         address: '1262 Mae Dr.',
         zip: '77015',
-        date: '',
+        date: new Date('10-01-2015'),
         hours: '9:00am-11:15am and 1:00pm-2:00pm'
     });
+	Events.insert(
+	    {
+	        name: 'Bonner Elementary',
+	        address: '8100 Elrod ',
+	        zip: '77017',
+	        date: new Date('10-22-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Bonner Elementary',
+	        address: '8100 Elrod ',
+	        zip: '77017',
+	        date: new Date('10-08-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Brookline Elementary',
+	        address: '6301 S. Loop East ',
+	        zip: '77087',
+	        date: new Date('10-27-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Children\'s Museum',
+	        address: '1500 Binz St. ',
+	        zip: '77004',
+	        date: new Date('10-08-2015'),
+	        hours: '5:00pm-7:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Community Family Centers',
+	        address: '7524 Ave E',
+	        zip: '77012',
+	        date: new Date('10-28-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Cornelius Elementary',
+	        address: '7475 Westover ',
+	        zip: '77087',
+	        date: new Date('10-05-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Golfcrest Elementary',
+	        address: '7414 Fairway ',
+	        zip: '77087',
+	        date: new Date('10-07-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Golfcrest Elementary',
+	        address: '7414 Fairway ',
+	        zip: '77087',
+	        date: new Date('10-21-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Hillendahl Library',
+	        address: '2436 Gessner Dr',
+	        zip: '77042',
+	        date: new Date('10-13-2015'),
+	        hours: '10:00am-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'RP Harris Elementary',
+	        address: '1262 Mae Dr. ',
+	        zip: '77015',
+	        date: new Date('10-15-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Lewis Elementary',
+	        address: '6745 Tipperary Lane',
+	        zip: '77061',
+	        date: new Date('10-06-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Lewis Elementary',
+	        address: '6745 Tipperary Lane',
+	        zip: '77061',
+	        date: new Date('10-20-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'McDonald\'s Spencer',
+	        address: '3223 Spencer Hwy.Pasadena',
+	        zip: '77504',
+	        date: new Date('10-19-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'North Channelview WIC',
+	        address: '13457 E Frwy Service Rd',
+	        zip: '77015',
+	        date: new Date('10-12-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Robinson Library',
+	        address: '3223 Wilcrest Dr ',
+	        zip: '77042',
+	        date: new Date('10-29-2015'),
+	        hours: '10:00am-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'UT Power WIC',
+	        address: '12401 1/2 S. Post Oak',
+	        zip: '77045',
+	        date: new Date('10-14-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
+	Events.insert(
+	    {
+	        name: 'Willow Creek Community',
+	        address: '7575 Office City Dr',
+	        zip: '77012',
+	        date: new Date('10-26-2015'),
+	        hours: '9:00am-11:15am and 1:00pm-2:00pm'
+	    });
 }
