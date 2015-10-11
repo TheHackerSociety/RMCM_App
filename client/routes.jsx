0739cdaf497832acc671c7bc09699bc8d847b9b6
Jimmie Butler <jimmiebtlr@gmail.com>
2015-10-11 12:33:00 -0500

    Add redirect from root to events index.

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,5 +1,5 @@
 const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
-const {Router, Route, Link, IndexRoute, history, History} = ReactRouter;
+const {Router, Route, Link, IndexRoute, history, History, Redirect} = ReactRouter;
 
 const browserHistory = history.createHistory();
 
@@ -7,19 +7,22 @@ App = React.createClass({
   render:function(){
     const { pathname } = this.props.location;
 
-    return (<div>
-      <ReactCSSTransitionGroup component="div" transitionEnterTimeout={500} transitionLeaveTimeout={300} transitionName="example" transitionAppear={true} transitionAppearTimeout={500} >
+    return (<div>{this.props.children}</div>);
+    /*  <ReactCSSTransitionGroup component="div" transitionEnterTimeout={500} transitionLeaveTimeout={300} transitionName="example" transitionAppear={true} transitionAppearTimeout={500} >
         {React.cloneElement(this.props.children || <div />, { key: pathname })}
       </ReactCSSTransitionGroup>
-    </div>);
+    </div>);*/
   }
 });
 
 Meteor.startup(function() {
   React.render((
     <Router history={browserHistory}>
-      <Route path="/events" component={EventsIndex} key='events'/>
-      <Route path="/events/:_id" component={EventsDetails} key='event'/>
+      <Redirect from="/" to="/events" />
+      <Route path="/" component={App}/>
+        <Route path="/events" component={EventsIndex} key='events'/>
+        <Route path="/events/:_id" component={EventsDetails} key='event'/>
+      <Route/>
     </Router>
   ),document.getElementsByTagName('body')[0]);
 });
