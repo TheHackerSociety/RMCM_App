27d32e2baa8f85c75dda6fd451866e79d684d41b
Jimmie Butler <jimmiebtlr@gmail.com>
2015-10-10 23:39:28 -0500

    Add services listing to details page

--- client/components/eventDetails.jsx
+++ client/components/eventDetails.jsx
@@ -16,6 +16,13 @@ EventsDetails = Styleable(React.createClass({
       margin: '20px',
       color: 'black',
       fontWeight: 'bold'
+    },
+    generalInfo: {
+      padding: '20px'
+    },
+    services: {
+      padding: '20px',
+      listStyleType: 'disc'
     }
   },
 
@@ -26,6 +33,26 @@ EventsDetails = Styleable(React.createClass({
     return (<div>
       <Link to='/events'><div style={this.styles('back')}>&#10092; Back</div></Link>
       <EventsSummary event={event}/>
+      <div style={this.styles('generalInfo')}>
+        <p>
+          Ronald McDonald Care Mobile <strong>FREE</strong> capabilities and pediatric
+          primary care services provided will vary by each community's needs and may
+          include:
+        </p>
+        <ul style={this.styles('services')}>
+          <li>Immunizations</li>
+          <li>Well-child visits</li>
+          <li>Ill or urgent care visits</li>
+          <li>Body Mass Index calculations and nutrition counseling</li>
+          <li>Developmental Screenings</li>
+          <li>Vision and hearing screening</li>
+          <li>Free medicine for uninsured children through on-site TCH pharmacy</li>
+          <li>School physicals - general and sports related</li>
+          <li>Blood collection and point of care testing</li>
+          <li>Childhood health education and injury prevention education.</li>
+          <li>Specialty care referrals including mental health and social service referrals</li>
+        </ul>
+      </div>
     </div>);
   }
 }));
